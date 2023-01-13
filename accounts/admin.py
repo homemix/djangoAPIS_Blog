14814d6d3343956e4875bd0280170cb1f1e9ea39
homemix <kenmutati@gmail.com>
2023-01-13 16:33:37 +0300

    Add: custom user authentication.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'email', 'username', 'name', 'is_staff',
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}),)
+    add_fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}),)
