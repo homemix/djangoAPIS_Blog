e01292a3106b5a4c8b676438c4b5394799fdcdce
homemix <kenmutati@gmail.com>
2023-01-15 12:03:47 +0300

    Add: user endpoints

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth import get_user_model
 from .models import Post
 
 
@@ -7,3 +7,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'body', 'created_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username']
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .models import Post
+from django.contrib.auth import get_user_model
 
 
 class PostList(generics.ListCreateAPIView):
@@ -14,3 +15,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
